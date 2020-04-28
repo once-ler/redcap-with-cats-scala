c0ec3fbab9bc171c3536cf14dc8e6d98f9489587
once-ler <htao@eztier.com>
2020-04-28 08:23:06 -0400

    Try export as upsert

--- redcap.entity/src/main/scala/com/eztier/redcap.entity/limsmock/infrastructure/api/REDCapInterpreter.scala
+++ redcap.entity/src/main/scala/com/eztier/redcap.entity/limsmock/infrastructure/api/REDCapInterpreter.scala
@@ -8,8 +8,9 @@ import cats.data.Chain
 import cats.effect.{ConcurrentEffect, ContextShift}
 import com.eztier.redcap.entity.limsmock.domain.types.{LimsSpecimen, RcDemographics}
 import fs2.{Pipe, Stream}
-import io.circe.Decoder
-import client.domain.{ApiAggregator, Project}
+import io.circe.{Decoder}
+import io.circe.syntax._
+import client.domain.{ApiAggregator, ApiOk, ApiResp, Project}
 
 class REDCapInterpreter[F[_]: Functor: ConcurrentEffect: ContextShift[?[_]]]
 (
@@ -18,42 +19,48 @@ class REDCapInterpreter[F[_]: Functor: ConcurrentEffect: ContextShift[?[_]]]
 
   import io.circe.generic.auto._
 
-  private def tryExport[A](options: Chain[(String, String)], in: A): Pipe[F, Option[String], Option[A]] =
+  private def tryExport[A](in: A, options: Chain[(String, String)]): Pipe[F, Option[String], ApiResp] =
     _.flatMap { a =>
-      apiAggregator.apiService.exportData[A](options)
+      val nextOptions = options ++ Chain("token" -> a.getOrElse(""))
+      
+      apiAggregator.apiService.exportData[A](nextOptions)
         .flatMap { r =>
-          val n = r match {
-            case Right(b) => b.some
+          r match {
+            case Right(b) =>
+              Stream.emit(ApiOk(b.asJson)).covary[F]
             case Left(e) =>
               println(e) // Log
-              None
+              apiAggregator.apiService.importData[A](in, nextOptions)
           }
-          Stream.emit(n).covary[F]
         }
     }
 
   def fetch[A](in: LimsSpecimen)(implicit ev: Decoder[A]) = {
     val partid = if (in.USE_STUDYLINKID.getOrElse(false)) in.STUDYLINKID.getOrElse("") else in.U_MRN.getOrElse("")
 
-    apiAggregator.createProject(
-      Project(
-        ProjectTitle = in.SSTUDYID,
-        Purpose = Some(4),
-        ProjectNotes = in.SSTUDYID,
-        ProjectIrbNumber = in.SSTUDYID
-      ), in.SSTUDYID
-    ).through(tryExport[RcDemographics](
-      Chain(
-        "content" -> "record",
-        "fields" -> "record_id,mrn,spc_id,lbv_id",
-        "forms" -> "demographics",
-        "filterLogic" -> s"[record_id] = '$partid'")
-      , RcDemographics(
-        RecordId = partid.some,
-        Mrn = in.U_MRN,
-        SpcId = in.STUDYLINKID
+    apiAggregator
+      .createProject(
+        Project(
+          ProjectTitle = in.SSTUDYID,
+          Purpose = Some(4),
+          ProjectNotes = in.SSTUDYID,
+          ProjectIrbNumber = in.SSTUDYID
+        ), in.SSTUDYID
+      ).through(
+        tryExport[RcDemographics](
+          RcDemographics(
+            RecordId = partid.some,
+            Mrn = in.U_MRN,
+            SpcId = in.STUDYLINKID
+          ),
+          Chain(
+            "content" -> "record",
+            "fields" -> "record_id,mrn,spc_id,lbv_id",
+            "forms" -> "demographics",
+            "filterLogic" -> s"[record_id] = '$partid'"
+          )
+        )
       )
-    ))
 
 
 
