279f19c324904b0b104506b8cf18460331b1f8eb
once-ler <htao@eztier.com>
2020-05-01 18:59:56 -0400

    Processing incoming records should be grouped

--- redcap.entity/src/test/scala/TestLimsSpecimenSpec.scala
+++ redcap.entity/src/test/scala/TestLimsSpecimenSpec.scala
@@ -9,7 +9,7 @@ import org.specs2.mutable.Specification
 import com.eztier.common.Util.csvToCC
 import com.eztier.common.{CSVConfig, CSVConverter}
 import com.eztier.redcap.client.createREDCapClientResource
-import com.eztier.redcap.entity.limsmock.domain.types.LimsSpecimen
+import com.eztier.redcap.entity.limsmock.domain.types.{LimsSpecimen, RcSpecimen}
 import com.eztier.redcap.entity.limsmock.createLvToRcAggregatorResource
 
 class TestLimsSpecimenSpec extends Specification {
@@ -19,15 +19,15 @@ class TestLimsSpecimenSpec extends Specification {
   implicit val cs = IO.contextShift(ec) // Need cats.effect.ContextShift[cats.effect.IO] because not inside of IOApp
 
   "REDCap Client Resource" should {
+
+    val csvFilePath = s"${System.getProperty("user.dir")}/../tmp/db/test.tsv"
+
     "Create usable client" in {
 
       createLvToRcAggregatorResource[IO].use {
         case lvToRcAggregator =>
-
-          val csvFilePath = s"${System.getProperty("user.dir")}/../tmp/db/test.tsv"
           // Read entire file.  Not a good idea.
           // val a = lvToRcAggregator.apiAggregator.apiService.readAllFromFile(csvFilePath).unsafeRunSync()
-
           val s = lvToRcAggregator.apiAggregator.apiService.readByLinesFromFile(csvFilePath)
 
           s.chunkN(10)
@@ -40,10 +40,10 @@ class TestLimsSpecimenSpec extends Specification {
                 .from(Some(a).fold("")(a => a))
                 .fold(_ => List[LimsSpecimen](), s => s)
 
+              println(l.map(_.SSTUDYID.get).mkString(","))
               println(l.length)
 
-              val r = lvToRcAggregator.limsSpecimenService.insertMany(l).unsafeRunSync()
-
+             //  val r = lvToRcAggregator.limsSpecimenService.insertMany(l).unsafeRunSync()
               Stream.emit(()).covary[IO]
             }.compile.drain.unsafeRunSync()
 
@@ -52,6 +52,39 @@ class TestLimsSpecimenSpec extends Specification {
 
       1 mustEqual 1
     }
+
+    "Convert LimsSpecimen to RcSpecimen" in {
+
+      createLvToRcAggregatorResource[IO].use {
+        case lvToRcAggregator =>
+
+          for {
+            x <- lvToRcAggregator.limsSpecimenService.list()
+            y = x.filter(_.REDCAPID.isDefined).groupBy(_.REDCAPID.get).toList
+            z = Stream.emits(y)
+              .flatMap { case (key, vals) =>
+
+                Stream.emits(vals).covary[IO]
+                  .chunkN(100)
+                  .flatMap { s =>
+
+                    s.map { t =>
+                      RcSpecimen(
+
+                      )
+                    }
+
+                    Stream.emit(()).covary[IO]
+                  }
+              }
+          } yield ()
+
+          IO.unit
+      }
+
+      1 mustEqual 1
+    }
+
   }
 }
 
